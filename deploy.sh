4f058463bee420e00a942a350680468ff0da323b
youssef <youss.hark@gmail.com>
2022-01-14 15:50:05 -0500

    Fixed some npm issues in the react client app

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
-docker build -t youssefh/multi-client:latest -t youssefh/multi-client:$SHA  -f ./client/Dockerfile ./client
-docker build -t youssefh/multi-server:latest -t youssefh/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t youssefh/multi-worker:latest -t -t youssefh/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t youssefh/multi-client:latest  -f ./client/Dockerfile ./client
+docker build -t youssefh/multi-server:latest  -f ./server/Dockerfile ./server
+docker build -t youssefh/multi-worker:latest  -f ./worker/Dockerfile ./worker
+docker build -t youssefh/multi-client:$SHA  -f ./client/Dockerfile ./client
+docker build -t youssefh/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t youssefh/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push yousseh/multi-client:latest
-docker push yousseh/multi-server:latest
-docker push yousseh/multi-worker:latest
+docker push youssefh/multi-client:latest
+docker push youssefh/multi-server:latest
+docker push youssefh/multi-worker:latest
 
-docker push yousseh/multi-client:$SHA
-docker push yousseh/multi-server:$SHA
-docker push yousseh/multi-worker:$SHA
+docker push youssefh/multi-client:$SHA
+docker push youssefh/multi-server:$SHA
+docker push youssefh/multi-worker:$SHA
 
 kubectl apply -f k8s/1
 kubectl apply -f k8s/2
